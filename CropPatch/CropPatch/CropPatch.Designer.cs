55c41457087c19e092f9b0e398d7e444b6cec93e
kuaitoukid <363724414@qq.com>
2016-09-12 21:08:48 +0800

    init
    init

---
+++ CropPatch/CropPatch/CropPatch.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CropPatch
+{
+    partial class CropPatch
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.picBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnOpenBrowser = new System.Windows.Forms.Button();
+            this.lblMousePosition = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBox
+            // 
+            this.picBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picBox.ErrorImage = null;
+            this.picBox.ImageLocation = "0, 0";
+            this.picBox.InitialImage = null;
+            this.picBox.Location = new System.Drawing.Point(12, 12);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(1246, 664);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBox.TabIndex = 0;
+            this.picBox.TabStop = false;
+            this.picBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picBox_MouseDown);
+            this.picBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picBox_MouseMove);
+            this.picBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picBox_MouseUp);
+            // 
+            // btnOpenBrowser
+            // 
+            this.btnOpenBrowser.Location = new System.Drawing.Point(1264, 12);
+            this.btnOpenBrowser.Name = "btnOpenBrowser";
+            this.btnOpenBrowser.Size = new System.Drawing.Size(142, 34);
+            this.btnOpenBrowser.TabIndex = 1;
+            this.btnOpenBrowser.Text = "SelectImages";
+            this.btnOpenBrowser.UseVisualStyleBackColor = true;
+            this.btnOpenBrowser.Click += new System.EventHandler(this.btnOpenBrowser_Click);
+            // 
+            // lblMousePosition
+            // 
+            this.lblMousePosition.AutoSize = true;
+            this.lblMousePosition.Location = new System.Drawing.Point(1291, 321);
+            this.lblMousePosition.Name = "lblMousePosition";
+            this.lblMousePosition.Size = new System.Drawing.Size(41, 12);
+            this.lblMousePosition.TabIndex = 2;
+            this.lblMousePosition.Text = "label1";
+            // 
+            // CropPatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1418, 688);
+            this.Controls.Add(this.lblMousePosition);
+            this.Controls.Add(this.btnOpenBrowser);
+            this.Controls.Add(this.picBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "CropPatch";
+            this.Text = "CropPatch";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.FolderBrowserDialog picBrowserDialog;
+        private System.Windows.Forms.Button btnOpenBrowser;
+        private System.Windows.Forms.Label lblMousePosition;
+    }
+}
+
---
+++ CropPatch/CropPatch/CropPatch.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CropPatch
+{
+    public partial class CropPatch : Form
+    {
+        public CropPatch()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> selectedFileNames = new List<string>();
+        private Image currentImage;
+        private float resizeRatio = 1;
+        private Graphics picBoxGraphics;
+        private Point startPoint;
+        private Point endPoint;
+        private Point ImageLeftTop;
+        private Point ImageRightBottom;
+
+        private void btnOpenBrowser_Click(object sender, EventArgs e)
+        {
+            // this.picBrowserDialog.ShowDialog();
+            this.selectedFileNames.Clear();
+            OpenFileDialog ofg = new OpenFileDialog();
+            ofg.Filter = "png(*.png), jpg(*.jpg), bmp(*.bmp)|*.png;*.jpg;*.bmp";
+            ofg.Title = "Select Images";
+            ofg.Multiselect = true;
+            if (ofg.ShowDialog() == DialogResult.OK)
+            {
+                for (int fid = 0; fid < ofg.FileNames.Length; fid++)
+			    {
+                    this.selectedFileNames.Add(ofg.FileNames[fid]);			 
+			    }
+            }
+            if (this.selectedFileNames.Count > 0) {
+                LoadImage(this.selectedFileNames[0]);
+            }
+
+        }
+
+        private void picBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.picBox.Refresh();
+            this.picBoxGraphics = this.picBox.CreateGraphics();
+            this.startPoint = new Point(e.X, e.Y);
+            this.endPoint = new Point(e.X, e.Y);
+
+            this.picBoxGraphics.DrawRectangle(new Pen(Color.FromArgb(255, 0, 0), 3), 
+                new Rectangle(Math.Min(this.startPoint.X, this.endPoint.X), Math.Min(this.startPoint.Y, this.endPoint.Y),
+                    Math.Abs(this.startPoint.X - this.endPoint.X + 1), Math.Abs(this.startPoint.Y - this.endPoint.Y + 1)));
+            this.picBoxGraphics.Dispose();
+        }
+
+        private void picBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.picBoxGraphics = this.picBox.CreateGraphics();
+            this.endPoint = new Point(e.X, e.Y);
+            this.picBoxGraphics.DrawRectangle(new Pen(Color.FromArgb(255, 0, 0), 3),
+                new Rectangle(Math.Min(this.startPoint.X, this.endPoint.X), Math.Min(this.startPoint.Y, this.endPoint.Y),
+                    Math.Abs(this.startPoint.X - this.endPoint.X + 1), Math.Abs(this.startPoint.Y - this.endPoint.Y + 1)));
+            this.picBoxGraphics.Dispose();
+        }
+
+        private void picBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                this.picBox.Refresh();
+                this.picBoxGraphics = this.picBox.CreateGraphics();
+                this.endPoint = new Point(e.X, e.Y);
+                this.picBoxGraphics.DrawRectangle(new Pen(Color.FromArgb(255, 0, 0), 3),
+                    new Rectangle(Math.Min(this.startPoint.X, this.endPoint.X), Math.Min(this.startPoint.Y, this.endPoint.Y),
+                        Math.Abs(this.startPoint.X - this.endPoint.X + 1), Math.Abs(this.startPoint.Y - this.endPoint.Y + 1)));
+                this.picBoxGraphics.Dispose();
+            }
+            this.lblMousePosition.Text = "X: " + e.X.ToString() + ", Y: " + e.Y.ToString();
+        }
+
+        private void LoadImage(string imageName)
+        {
+            this.currentImage = Image.FromFile(imageName);
+            this.resizeRatio = 1.0f * Math.Max(this.picBox.Height, this.picBox.Width) / Math.Max(this.currentImage.Height, this.currentImage.Width);
+            this.picBox.Image = this.currentImage;
+            int centerX = this.picBox.Width / 2;
+            int centerY = this.picBox.Height / 2;
+            int halfWidth = (int)(this.resizeRatio * this.currentImage.Width / 2);
+            int halfHeight = (int)(this.resizeRatio * this.currentImage.Height / 2);
+            
+            this.ImageLeftTop = new Point(centerX - halfWidth, centerY - halfHeight);
+            this.ImageRightBottom = new Point(centerX + halfWidth, centerY + halfHeight);
+
+
+        }
+    }
+}
